085d9b6ed74a0693acf20c1fbd59b417334addc0
s1310238069@students.fh-hagenberg.at <michaeltroger@users.noreply.github.com>
2016-10-06 00:34:49 +0200

    copyright added

--- home-esp8266-temperature.ino
+++ home-esp8266-temperature.ino
@@ -1,3 +1,20 @@
+/**
+Copyright 2016 Michael Troger <https://michaeltroger.com>
+This file is part of Home Surveillance with Node.js.
+
+Home Surveillance with Node.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Home Surveillance with Node.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Home Surveillance with Node.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
